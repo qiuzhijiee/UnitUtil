bdcd8fa1e4a48e2b57ec31f0e25774f6fd302d9b
qiu <qiuzhijiee@gmail.com>
2019-12-10 11:01:23 +0800

    1

--- src/com/likeStudy/unitArithmetic/UnitArithmetic.java
+++ src/com/likeStudy/unitArithmetic/UnitArithmetic.java
@@ -1,6 +1,21 @@
 package com.likeStudy.unitArithmetic;
 
-public abstract class UnitArithmetic {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UnitArithmetic {
+
+	//正则表达式
+	private String pattern;
+	private String toUnit;
+	
+	public UnitArithmetic(String originUnit, String toUnit) {
+		pattern = "([0-9.]*+"+originUnit+")";
+		this.toUnit = toUnit.trim();
+	}
+	
+	
+	
 	/**
 	 * 
 	 * @param line
@@ -10,5 +25,24 @@ public abstract class UnitArithmetic {
 	 * @return
 	 * 	将转换好的String返回
 	 */
-	abstract public String arithmetic(String line, float unit);
+	public String arithmetic(String line, float unit) {
+		if(pattern == null || pattern.equals("") || toUnit == null || toUnit.equals("")) {
+			System.out.println("属性异常");
+			return null;
+		}
+		
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		
+		while(m.find()) {
+			String result = m.group(1);
+			float f = Float.parseFloat(result.substring(0, result.length()-toUnit.length())) * unit;
+			line = m.replaceFirst(f+toUnit);
+			
+			//需要重新给Matcher赋值，上面使用的是replaceFirst，不重新赋值如果有多个就会一直更改第一个(死循环)
+			//可能有更好的替换方法，目前只知道replaceFirst 和 replaceAll  替换第一个和替换全部
+			m = r.matcher(line);
+		}
+		return line;
+	}
 }
---
+++ src/com/likeStudy/util/FileUtil.java
@@ -0,0 +1,37 @@
+package com.likeStudy.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class FileUtil {
+	public static File createFile(String path) {
+		File file = new File(path);
+		if(file.isDirectory()) {
+			System.out.println("传入路径为文件夹");
+			return null;
+			//throw new Exception("传入路径为文件夹");
+		}
+		if(!file.exists()) {
+			file.getParentFile().mkdirs();
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return file;
+	}
+	
+	
+	@Test
+	public void test1() throws Exception {
+		try {
+		File file = createFile("D:\\myProject\\TestFIle\\demo");
+		System.out.println(file.getName());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
